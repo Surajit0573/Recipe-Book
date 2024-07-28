8456246e467c553bcfc97600b1760c780cdb7e31
Surajit Maity <sibu23122003@gmail.com>
2024-07-29 00:35:13 +0530

    small update

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ main().then(() => {
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/RecipeBook');
+  await mongoose.connect(process.env.MONGODB_URL);
 
 }
 
